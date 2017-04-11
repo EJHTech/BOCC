5d015b165737322b3da1e30d4fd33a74f8f63a4d
EJHTech <abiogenesis@gmail.com>
2017-04-11 19:58:14 +0100

    Ready for Delivery
    Silenced unnecessary verbose error messages and updated README

--- README.md
+++ README.md
@@ -8,26 +8,27 @@ BOCC is the Blackwells Online Course Collector
 
 Installation:
   -For Linux and MacOS:
-    1. Visit https://github.com/mrknickerbocker/BOCC
-       or svn https://github.com/mrknickerbocker/BOCC.git
-    2. "Clone or download" the files
-    3. Unzip/move to the desired directory.
+    Visit https://github.com/mrknickerbocker/BOCC
+    "Clone or download" the files
+
+    Unzip in the desired directory.
+
+    The program can be run in the directory it is stored in by using
+    'chmod +x *.sh'
+    to make the files executable, then
+    './bocc.sh' to run
 
   -For Windows:
-    1. This code is based on a shell script.  As such, to work on Windows it requires
+    This code is based on a shell script.  As such, to work on Windows it requires
     the installation of a linux terminal emulator. It has been tested on Cygwin (on Windows 7).
     You can find Cygwin and instructions for installation here: https://cygwin.com/install.html
       In addition to the base installation, the "wget" module must be installed.
-    2. Within Cygwin (or other emulator), follow instructions for Linux and MacOS
+      Afterwards, continue installation as in the "For Linux and MacOS" section
 
 Execution:
-
-  The program can be run in the directory it is stored in by using
-  'chmod +x *.sh'
-  to make the files executable, then
-  './bocc.sh' to run
-
   After running the program, you will be greeted and prompted to choose the
   appropriate school and then the relevant year.  The program will then run, and
   inform you when it has finished.  The output, including logs, will be stored
-  in subfolders, first by school name and then by year.
+  in subfolders, first by school name and then by year.  There will be a variety of files
+  created, but you will be most interested in the OETTDSYtrimmed.txt and DRPSreadingList.txt
+  files.
--- edinburgh.sh
+++ edinburgh.sh
@@ -3,7 +3,6 @@
 #Blackwell's Online Course Collector
 #Version 0.1
 # Copyright 2017 Eric Hofrichter
-# Time-stamp: <2017-02-16 13:44:07>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,36 +20,30 @@
 
 echo "Edinburgh";
 SCHOOL=Edinburgh
-currentdir=$PWD
+current_dir=$PWD
 
 year=`echo $1 | sed 's/20//g'`
 
 echo "Downloading Information.  This will take a while"
 mkdir -p $SCHOOL/$year
-mkdir $SCHOOL/$year/bak
 
 #download course information----------------------------------------------------
 
-#wget -r --level=4 -e robots=off  --accept=htm --no-check-certificate -nc -np www.drps.ed.ac.uk/$year
-
-#Backup files
-mkdir www.drps.ed.ac.uk/$year/bak
-cp -n -R www.drps.ed.ac.uk/$year/dpt/ www.drps.ed.ac.uk/$year/bak/
+wget -r --level=4 -e robots=off  --accept=htm --reject-regex '[a-z].htm' --no-check-certificate -nc -np www.drps.ed.ac.uk/$year
 
 #Remove extraneous information--------------------------------------------------
-rm www.drps.ed.ac.uk/$year/dpt/cx_*.htm
-for alpha in {d,p,u,v}; do
-  rm www.drps.ed.ac.uk/$year/dpt/$alpha*.htm
-done
-
+rm www.drps.ed.ac.uk/$year/dpt/*[^0-9].htm 2>> log.txt
+rm www.ed.ac.uk/profile/*student 2>> log.txt
 for alpha in {a..z}; do
-  sed -i -e '/Course secretary/,+2d' www.drps.ed.ac.uk/$year/dpt/cx$alpha*.htm
+  sed -i -e '/Course secretary/,+2d' www.drps.ed.ac.uk/$year/dpt/cx$alpha*.htm 2>> log.txt
 done
 
-#change working directory-------------------------------------------------------
+#change working directory
 cd $SCHOOL/$year
 
+#Strip out, sort, clean up, and join names and emails (from profile pages)------
 echo "Working on:"
+echo "Emails..."
 
 #Strip out and clean up course organizer (from course pages)--------------------
 echo "Courses..."
@@ -59,16 +52,16 @@ find ../../www.drps.ed.ac.uk/$year/dpt/ -type f | xargs grep -H "Course organise
 find ../../www.drps.ed.ac.uk/$year/dpt/ -type f | xargs grep -H -L "Course organiser<" > NOcourseOrganiser.txt
 sed -i 's/<td class="rowhead1"  width="15%">Course organiser<\/td><td width="35%">/^/g' courseOrganiser.txt
 sed -i 's/<br>/^/g' courseOrganiser.txt
-sort courseOrganiser.txt > SORTEDCOURSEORGANISER.txt
-
+sort courseOrganiser.txt > COURSEORGANISER.txt
+exit
 #strip out and clean up telephone-----------------------------------------------
 echo "Telephone Numbers..."
-find ../../www.drps.ed.ac.uk/$year/dpt/ -type f | xargs grep -H "<b>Tel:</b> " > Tel.txt
-find ../../www.drps.ed.ac.uk/$year/dpt/ -type f | xargs grep -H -L "<b>Tel:</b> " > NOTel.txt
+find ../../www.drps.ed.ac.uk/$year/dpt/ -type f | xargs grep -H "Tel" > Tel.txt
+find ../../www.drps.ed.ac.uk/$year/dpt/ -type f | xargs grep -H -L "Tel" > NOTel.txt
 sed -i 's/<b>Tel:<\/b>/^/g' Tel.txt
 sed -i 's/<br>//g' Tel.txt
 sed -i 's/ (/(/g' Tel.txt
-sort Tel.txt > SORTEDTEL.txt
+sort Tel.txt > TEL.txt
 
 
 #strip out and clean up email address-------------------------------------------
@@ -76,14 +69,14 @@ echo "Email Addresses..."
 find ../../www.drps.ed.ac.uk/$year/dpt/ -type f | xargs grep -H "Email:" > Email.txt
 find ../../www.drps.ed.ac.uk/$year/dpt/ -type f | xargs grep -H -L "Email:" > NOEmail.txt
 sed -i 's/<b>Email:<\/b> /^/g' Email.txt
-sed 's/<\/td>//g' Email.txt > SORTEDEMAIL.txt
+sed 's/<\/td>//g' Email.txt > EMAIL.txt
 
 #strip out and clean up course title/number
 echo "Course Titles and Numbers..."
 find ../../www.drps.ed.ac.uk/$year/dpt/ -type f | xargs grep "<title>Course Catalog" > titleAndDRPS.txt
 sed -i 's/<title>Course Catalogue - /^/g' titleAndDRPS.txt
 sed -i 's/<\/title>//g' titleAndDRPS.txt
-
+#sed -i 's/ (/^(/g' titleAndDRPS.txt
 
 #split DRPS and title-----------------------------------------------------------
 grep ') (' titleAndDRPS.txt > twoParens.txt
@@ -92,8 +85,8 @@ sed -i '/) (/d' titleAndDRPS.txt
 sed -i 's/ (/^(/g' titleAndDRPS.txt
 cat twoParens.txt >> titleAndDRPS.txt
 sort titleAndDRPS.txt | uniq > uniqTitleAndDRPS.txt
-cut -d ^ -f 1,3 uniqTitleAndDRPS.txt > SORTEDDRPS.txt
-cut -d ^ -f 1,2 uniqTitleAndDRPS.txt > SORTEDTITLES.txt
+cut -d ^ -f 1,3 uniqTitleAndDRPS.txt > DRPS.txt
+cut -d ^ -f 1,2 uniqTitleAndDRPS.txt > TITLES.txt
 
 #strip out and clean up course Semester (course start)--------------------------
 echo "Semesters..."
@@ -105,7 +98,7 @@ grep colspan courseStart.txt > semester.txt
 sort semester.txt > sortedSemester.txt
 uniq sortedSemester.txt > uniqSemester.txt
 sed -i 's/<\/td>//g' uniqSemester.txt
-sed 's/-<td colspan="14">/:^/g' uniqSemester.txt > SORTEDSEMESTER.txt
+sed 's/-<td colspan="14">/:^/g' uniqSemester.txt > SEMESTER.txt
 
 #strip out and clean up year (SCQF Level 7 (year 1 Undergraduate))--------------
 echo "year..."
@@ -113,19 +106,19 @@ find ../../www.drps.ed.ac.uk/$year/dpt/ -type f | xargs grep 'Credit level' > ye
 find ../../www.drps.ed.ac.uk/$year/dpt/ -type f | xargs grep -L 'Credit level' > NOyear.txt
 sed -i 's/<\/td>//g' year.txt
 sed -i 's/<td.*SCQF/^SCQF/g' year.txt
-sort year.txt > SORTEDYEAR.txt
-
+sort year.txt > sortedYear.txt
+mv sortedYear.txt YEAR.txt
 
 #join on file names-------------------------------------------------------------
 export LC_ALL='C'
 
 echo "Joining fields..."
-join -t ^ -1 1 SORTEDCOURSEORGANISER.txt SORTEDEMAIL.txt > OE.txt
-join -t ^ -1 1 OE.txt SORTEDTEL.txt > OET.txt
-join -t ^ -1 1 OET.txt SORTEDTITLES.txt > OETT.txt
-join -t ^ -1 1 OETT.txt SORTEDDRPS.txt > OETTD.txt
-join -t ^ -1 1 OETTD.txt SORTEDSEMESTER.txt > OETTDS.txt
-join -t ^ -1 1 OETTDS.txt SORTEDYEAR.txt > OETTDSY.txt
+join -t ^ -1 1 COURSEORGANISER.txt EMAIL.txt > OE.txt
+join -t ^ -1 1 OE.txt TEL.txt > OET.txt
+join -t ^ -1 1 OET.txt TITLES.txt > OETT.txt
+join -t ^ -1 1 OETT.txt DRPS.txt > OETTD.txt
+join -t ^ -1 1 OETTD.txt SEMESTER.txt > OETTDS.txt
+join -t ^ -1 1 OETTDS.txt YEAR.txt > OETTDSY.txt
 tr -s "^" < OETTDSY.txt > OETTDSYtrimmed.txt
 
 #strip out reading list and prepend filename------------------------------------
@@ -133,12 +126,16 @@ tr -s "^" < OETTDSY.txt > OETTDSYtrimmed.txt
 echo "Reading List..."
 
 for alpha in {a..z}; do
-gawk -i inplace -v INPLACE_SUFFIX=.bak '/Reading List/{flag=1;next}/table/{flag=0;next} flag' ../../www.drps.ed.ac.uk/$year/dpt/cx[$alpha]*.htm
+cp -n ../../www.drps.ed.ac.uk/$year/dpt/cx[$alpha]*.htm ../../www.drps.ed.ac.uk/$year/bak 2>> log.txt
+done
+
+for alpha in {a..z}; do
+gawk -i inplace -v INPLACE_SUFFIX=.bak '/Reading List/{flag=1;next}/table/{flag=0;next} flag' ../../www.drps.ed.ac.uk/$year/dpt/cx[$alpha]*.htm 2>> log.txt
 done
 
 #add full path to filename for join matching and create readingList file--------
 for alpha in {a..z}; do
-awk '{print FILENAME,$0}' ../../www.drps.ed.ac.uk/$year/dpt/cx[$alpha]*.htm >> readingList.txt
+awk '{print FILENAME,$0}' ../../www.drps.ed.ac.uk/$year/dpt/cx[$alpha]*.htm > readingList.txt 2>> log.txt
 done
 
 #clean up readingList-----------------------------------------------------------
@@ -149,23 +146,17 @@ sed -i '/htm:\^[ ]*$/d' readingList.txt
 sed -i 's/\^ [0-9]*\./^ /g' readingList.txt
 tr -s "-" < readingList.txt > readingList2.txt
 mv readingList2.txt readingList.txt
-join -t ^ -1 1 SORTEDDRPS.txt readingList.txt > DRPSreadingList.txt
-iconv -f utf-8 -t utf-8 -c DRPSreadingList.txt > utf8DRPSreadingList.txt
+join -t ^ -1 1 DRPS.txt readingList.txt > DRPSreadingList.txt
 
 
 
 #clean up folder
 echo "Cleaning up..."
-for alpha in {d..z}; do
-rm -R ../../www.drps.ed.ac.uk/$year/dpt/$alpha*
-rm -R ../../www.drps.ed.ac.uk/$year/dpt/cx$alpha*
-done
-rm -R ../../www.drps.ed.ac.uk/$year/dpt/*
-
-cp -R ../../www.drps.ed.ac.uk/$year/bak/ ../../www.drps.ed.ac.uk/$year/dpt/
+rm -R ../../www.drps.ed.ac.uk/$year/dpt/ 2>> log.txt
+cp -R ../../www.drps.ed.ac.uk/$year/bak/ ../../www.drps.ed.ac.uk/$year/dpt/ 2>> log.txt
 echo "Done"
 
 #return to BOCC directory
-cd $currentdir
+cd $current_dir
 
 exit
